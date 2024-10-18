a877fc019dca804cbc37057da15f2ce61ecdb5a9
DeepakRajasekaran <rajasekarand375@gmail.com>
2024-10-18 22:51:41 +0530

    Activity004 client update

--- study_ws/src/py_pkg/py_pkg/activity004_client.py
+++ study_ws/src/py_pkg/py_pkg/activity004_client.py
@@ -1,25 +1,69 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from std_srvs.srv import SetBool
+from irobot_interfaces.srv import SetLed
+
+# Python Specifics..
+import random
+from functools import partial
 
 
-class battery_node(Node): 
+class BatteryNode(Node): 
     def __init__(self):
         super().__init__("battery_node")
+        self.get_logger().info('Client has initiated...')
+        self.client_ = self.create_client(SetLed, 'set_led')
+        while not self.client_.wait_for_service(timeout_sec=2.0):
+            self.get_logger().warn('Waiting for Server to start....')
+        self.request_timer = self.create_timer(1.0, self.request_)
 
-        self.client_ = self.create_client(SetBool, 'set_led', self.request)
+    def battery_percent(self):
+        return random.randint(1, 100)
 
-    def request():
-        pass
+    def process_led_states(self, array, charge):
+        match charge:
+            case x if x < 25:
+                array = [False] * len(array)
+            case x if x < 50:
+                array[0] = True
+                array[1:] = [False] * (len(array) - 1)
+            case x if x < 75:
+                array[0] = True
+                array[1] = True
+                array[2:] = [False] * (len(array) - 2)
+            case x if 75 < x < 95:
+                array[0] = True
+                array[1] = True
+                array[2] = True
+                array[3:] = [False] * (len(array) - 3)
+            case x if x >= 95:
+                array = [True] * len(array)
+        return array
 
+    def request_(self):
+        request_call = SetLed.Request()
+        request_call.input_array = [False] * 4
+        request_call.input_array = self.process_led_states(request_call.input_array, self.battery_percent())
+
+        self.get_logger().info(f'Determined LED States: {request_call.input_array}')
+        self.future = self.client_.call_async(request_call)
+        self.future.add_done_callback(partial(self.response_callback, request_call))
+
+    def response_callback(self, request, future):
+        try:
+            response = future.result()
+            if response.success:
+                self.get_logger().info('LED Array Updated.')
+            else:
+                self.get_logger().warn('LED Array Update Failed.')
+        except Exception as e:
+            self.get_logger().error(f"Service call failed: {e}")
 
 def main(args=None):
     rclpy.init(args=args)
-    node = battery_node() 
+    node = BatteryNode() 
     rclpy.spin(node)
     rclpy.shutdown()
-
 
 if __name__ == "__main__":
     main()
