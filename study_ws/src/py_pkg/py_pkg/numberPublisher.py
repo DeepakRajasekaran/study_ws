13a92e1befa6fb7f91c1b1c0d739e01e48a2c58c
DeepakRajasekaran <rajasekarand375@gmail.com>
2024-10-21 18:35:24 +0530

    ros2_parameters with cpp&py init

--- study_ws/src/py_pkg/py_pkg/numberPublisher.py
+++ study_ws/src/py_pkg/py_pkg/numberPublisher.py
@@ -13,21 +13,35 @@ class numPub(Node):
     def __init__(self):
         super().__init__(NodeName)
         self.get_logger().info('NumberPublisher Has been initiated')
+        # Tried ros2Parameters in this region..
+        self.declare_parameter('number_to_publish', 10)
+        self.declare_parameter('publishFrequency', 1)
+        self.period = 1/self.get_parameter('publishFrequency').value
         self.number_publisher_ = self.create_publisher(Int64, 'number', 10)
-        self.timer_callback = self.create_timer(1.0, self.publishNumber)
+        self.timer_callback = self.create_timer(self.period, self.publishNumber)
     
     def publishNumber(self):
         msg = Int64()
-        msg.data = random.randint(1, 100)
-        self.get_logger().info('Publishing number :' + str(msg.data))
-        self.number_publisher_.publish(msg)
+        #random.randint(1, 100)
+        try:
+            msg.data = self.get_parameter('number_to_publish').value
+            self.get_logger().info('Publishing number :' + str(msg.data))
+            self.number_publisher_.publish(msg)            
+        except Exception as e:
+            self.get_logger().error(f'Ran into an error : {e}')
 
 def main(args=None):
     rclpy.init(args=args)
-    node = numPub() 
-    rclpy.spin(node)
-    node.destroy_node()
-    rclpy.shutdown()
+    node = numPub()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        node.get_logger().info('Detected KeyboardInterrupt. Exiting...')
+    finally:
+        # Clean up the node and shutdown rclpy
+        if rclpy.ok():
+            node.destroy_node()
+            rclpy.shutdown()
 
 
 if __name__ == "__main__":
