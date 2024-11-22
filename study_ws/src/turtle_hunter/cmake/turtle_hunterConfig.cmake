c624f87672c54fdf5234086b315971c01cf4f74b
DeepakRajasekaran <rajasekarand375@gmail.com>
2024-11-22 18:24:07 +0530

    working on launch files

---
+++ study_ws/src/turtle_hunter/cmake/turtle_hunterConfig.cmake
@@ -0,0 +1,1 @@
+set(turtle_hunter_DIR "${CMAKE_CURRENT_LIST_DIR}")
--- study_ws/src/turtle_hunter/setup.py
+++ study_ws/src/turtle_hunter/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['cmake/turtle_hunterConfig.cmake']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
